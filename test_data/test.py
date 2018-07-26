0deda8e16a99ca10d7e1de77ac9a10fd8a9783b3
Dương Tấn Sang <1512777@hcmut.edu.vn>
2018-07-26 10:58:34 +0700

    hihi

--- test_data/test.py
+++ test_data/test.py
@@ -1,17 +1,37 @@
 import cv2
-import numpy as np
-
-from main.feature.image_preprocessor import scale_to_fill
-
 
 img_not_scale = cv2.imread('not_scale.png',cv2.IMREAD_GRAYSCALE)
 
+height, width = img_not_scale.shape[:2]
 
-test = scale_to_fill(img_not_scale)
+# Get extreem values from the image
+max_x = 0
+min_x = height
+max_y = 0
+min_y = width
+
+for x in range(0, height):
+    for y in range(0, width):
+
+        if img_not_scale[x][y] != 255:
+            if x > max_x:
+                max_x = x
+            if x < min_x:
+                min_x = x
+            if y > max_y:
+                max_y = y
+            if y < min_y:
+                min_y = y
+# Cut out the part of image containing colored pixels
+sub_image = img_not_scale[min_y:max_y, min_x:max_x]
+
+# Scale the image
+resize_image = cv2.resize(sub_image, (width, height), interpolation=cv2.INTER_CUBIC)
+
+cv2.imshow('c', img_not_scale)
+cv2.imshow('a', sub_image)
+cv2.imshow('b', resize_image)
 
 
-
-cv2.imshow('b',img_not_scale)
-cv2.imshow('a',test)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
